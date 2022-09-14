65d532efed8b43a98c20009caa8ebffe2b173972
Issac Cabral <issaccabrall@gmail.com>
2022-09-14 17:08:30 -0300

    feat(aut): implement local strategy validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(private userService: UserService){}
+
+    async validateUser(email: string, password: string) {
+        const user = await this.userService.findByEmail(email)
+
+        if(user){
+            // checar se a senha informada corresponde a hash que está no banco
+            const isPasswordValid = await bcrypt.compare(password, user.password)
+            
+            if(isPasswordValid){
+                return {
+                    ...user,
+                    password: undefined
+                }
+            }
+        } 
+
+        throw new Error('invalid credentials')
+    }
+}
